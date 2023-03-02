553635a0c3b8f8544df0707578ca77d990436deb
Javier Martinez <hoogmin@gmail.com>
2023-03-01 20:02:44 -0500

    Site's start.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css'
+import 'highlight.js/styles/base16/spacemacs.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
