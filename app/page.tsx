172b7b26decc6da8745140250224113c0e371f18
Javier Martinez <hoogmin@gmail.com>
2024-04-13 22:11:58 -0400

    New profile, purpose, and skills section.

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,17 @@ import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faCodeBranch } from '@fortawesome/free-solid-svg-icons'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faXTwitter } from '@fortawesome/free-brands-svg-icons'
 import Link from "next/link"
 
 export const metadata: Metadata = {
-  title: "Home | Javi M.",
-  description: "Javier Martinez (@hoogmin) | Coder/Programmer.",
+  title: "Home | Javier M.",
+  description: "Javier Martinez (@hoogmin) | Freelance Coder",
   viewport: "width=device-width, initial-scale=1",
   robots: "all",
   openGraph: {
-    title: "Home | Javi M.",
-    description: "Javier Martinez (@hoogmin) | Coder/Programmer."
+    title: "Home | Javier M.",
+    description: "Javier Martinez (@hoogmin) | Freelance Coder"
   }
 }
 
@@ -26,7 +26,7 @@ export default function HomePage() {
           <div className="col-md-4 text-center">
             <div className="profile-picture">
               <Image
-                src="/profile1_nodata_site.jpg"
+                src="/profile2_clean_square.jpg"
                 alt="Javier M Picture"
                 width={141}
                 height={141}
@@ -36,22 +36,22 @@ export default function HomePage() {
             </div>
             <div className="mt-3">
               <h4>Javier M.</h4>
+              <p>Freelance Web Developer</p>
             </div>
           </div>
           <div className="col-md-8">
             <div className="mt-3 mt-md-0">
               <h2>About Me</h2>
               <p>
-                Welcome. I&apos;m Javi. I like things coding and technology related. I mainly do
-                web development &#40;JavaScript/TypeScript, React, etc&#41;. I also like Python
-                and PowerShell, Both of which I have used for general automation. GNU/Linux is an
-                operating system I have been passionate about since my teens and even used it as my
-                main system at one point. I am a self-learning type, meaning I prefer to learn via online
-                courses &#40;YouTube, Udemy, etc&#41;, books, and actual projects. I first began my journey in my early
-                teens learning the C and Javascript languages with the intention of building games with them. I
-                would say the ability to figure out and apply concepts quickly has carried me a lot. Thanks
-                for checking out my site. I built it using Next.js and TypeScript. Feel free to check out
-                other things I have worked on in the <Link href="/projects"><FontAwesomeIcon icon={faCodeBranch}/>projects</Link> section.
+                Welcome. I'm Javier. I am a freelance web developer whose mission is to
+                take your vision and make it reality. I am Full-Stack, meaning I can handle
+                tasks from how your website/webapp &#40;Front-End&#41; looks to the code powering its functionality behind the
+                scenes &#40;Back-End&#41;. I have experience building static websites, backend APIs, and complete web applications.
+                I am also very familiar with Linux and creating API integrations between different applications. You can find some
+                of the projects I have worked on in the <Link href="/projects"><FontAwesomeIcon icon={faCodeBranch}/>Projects</Link> page.
+                Interested? Email me at <a href="mailto:javier.martinezbz@gmail.com">javier.martinezbz@gmail.com</a> or message me on&nbsp;
+                <a href="https://www.upwork.com/freelancers/~01fae346cb0e37280b" rel="noopener noreferrer" target="_blank">Upwork</a>.
+                Thanks!
               </p>
               <h2>Contact&#47;Social</h2>
               <ul>
@@ -68,6 +68,25 @@ export default function HomePage() {
                 style={{ marginRight: 5 }}
                 />
                   GitHub: <a href="https://github.com/hoogmin" target="_blank" rel="noopener noreferrer">@hoogmin</a>
+                </li>
+                <li>
+                <FontAwesomeIcon
+                icon={faXTwitter}
+                style={{ marginRight: 5 }}
+                />
+                  Twitter/X: <a href="https://twitter.com/hexjavi" target="_blank" rel="noopener noreferrer">@hexjavi</a>
+                </li>
+              </ul>
+              <h2>Skills Overview</h2>
+              <ul>
+                <li>HTML/CSS/JS, JavaScript/TypeScript, React.js, Next.js, Bootstrap</li>
+                <li>Node.js, Express</li>
+                <li>C#, ASP.NET Core, Razor Pages</li>
+                <li>Unix-Like Systems like Linux and FreeBSD</li>
+                <li>Databases: Relational and Non-relational such as MySQL and MongoDB</li>
+                <li>
+                  Punctual, responsive, and honest. I show you my progress and I don't flake on people.
+                  If there is an issue, you can count on me being communicative about it.
                 </li>
               </ul>
             </div>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,7 +5,7 @@ import PortfolioCarousel from "@/components/PortfolioCarousel"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
-    title: "Projects | Javi M.",
+    title: "Projects | Javier M.",
     description: "Projects | Javier Martinez (@hoogmin) | Coder/Programmer.",
     viewport: "width=device-width, initial-scale=1",
     robots: "all",
@@ -35,72 +35,6 @@ const ProjectsPage = () => {
                 </h6>
             </header>
             <main className="container">
-                {/* <ol className="centered-list">
-                    <li>
-                        <a href="https://github.com/hoogmin/EnergizingMiniQuest" target="_blank" rel="noopener noreferrer">Energizing Mini Quest</a>
-                        : C#&#47;Unity Game made in 72 Hours.
-                        &#40;
-                        <a href="https://gamejolt.com/games/ld39_energizing_mini_quest/273245" target="_blank" rel="noopener noreferrer">Release Page</a>
-                        &#41;
-                    </li>
-                    <li>
-                        <a href="https://github.com/hoogmin/Chaos-Cell" target="_blank" rel="noopener noreferrer">Chaos Cell</a>
-                        : C#&#47;Unity Game made in 48 Hours.
-                        &#40;
-                        <a href="https://gamejolt.com/games/chaos-cell-ld40/301301" target="_blank" rel="noopener noreferrer">Release Page</a>
-                        &#41;
-                    </li>
-                    <li>
-                        <a href="https://www.infinityrfg.net" target="_blank" rel="noopener noreferrer">Infinity Roofing &amp; Sheet Metal</a>
-                        : Website for a roofing company created with a content management system &#40;Wix&#41;.
-                    </li>
-                    <li>
-                        <a href="https://github.com/hoogmin/arkaclone" target="_blank" rel="noopener noreferrer">Arkaclone</a>
-                        : A 3D clone of the classic game Arkanoid/Break The Bricks built with three.js playable in the browser.
-                    </li>
-                    <li>
-                        <Link href="/" target="_blank" rel="noopener noreferrer">My own site</Link>
-                        : Built using Next.js, React, and TypeScript.
-                    </li>
-                </ol> */}
-                {/* <div id="portfolioCarousel" className="carousel slide">
-                    <div className="carousel-indicators">
-                        <button type="button" data-bs-target="#portfolioCarousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#portfolioCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#portfolioCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                    </div>
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <img src="/portfolio/innotech.png" className="d-block w-100" alt="Innotech Security, Inc."/>
-                                <div className="carousel-caption d-none d-md-block">
-                                    <h5>First slide label</h5>
-                                    <p>Some representative placeholder content for the first slide.</p>
-                                </div>
-                        </div>
-                        <div className="carousel-item">
-                            <img src="/portfolio/infinityrfg.png" className="d-block w-100" alt="Infinity Roofing & Sheet Metal, Inc."/>
-                                <div className="carousel-caption d-none d-md-block">
-                                    <h5>Second slide label</h5>
-                                    <p>Some representative placeholder content for the second slide.</p>
-                                </div>
-                        </div>
-                        <div className="carousel-item">
-                            <img src="/portfolio/chaos_cell.png" className="d-block w-100" alt="Chaos Cell Ludum Dare"/>
-                                <div className="carousel-caption d-none d-md-block">
-                                    <h5>Third slide label</h5>
-                                    <p>Some representative placeholder content for the third slide.</p>
-                                </div>
-                        </div>
-                    </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#portfolioCarousel" data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#portfolioCarousel" data-bs-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
-                </div> */}
                 <PortfolioCarousel/>
             </main>
         </>
